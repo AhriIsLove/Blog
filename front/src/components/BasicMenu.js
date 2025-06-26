983e2752083b32d853e95ee55cc90e5cd3649905
AhriIsLove <60705922+AhriIsLove@users.noreply.github.com>
2025-06-26 23:29:04 +0900

    css update
    tailwind방식 적용

--- front/src/components/BasicMenu.js
+++ front/src/components/BasicMenu.js
@@ -1,15 +1,15 @@
 import { Link } from "react-router-dom";
-import '../css/BasicCss.css';
+import '../index.css';
 
 const BasicMenu = () => {
     return (
         <nav id="navbar" className="flex bg-blue-300">
             <div className="w-4/5 bg-gray-500">
                 <ul className="flex p-4 text-white font-bold">
-                    <li className="Menu">
+                    <li className="menu">
                         <Link to={'/'}>Main</Link>
                     </li>
-                    <li className="Menu">
+                    <li className="menu">
                         <Link to={'/about'}>About</Link>
                     </li>
                 </ul>
