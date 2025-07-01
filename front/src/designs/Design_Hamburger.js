ad3b1d274e29eb98282c57a69dcf17df38a02b4b
AhriIsLove <60705922+AhriIsLove@users.noreply.github.com>
2025-07-02 07:18:26 +0900

    CSS HamburgerMenu update
    햄버거버튼 애니메이션 추가

--- front/src/designs/Design_Hamburger.js
+++ front/src/designs/Design_Hamburger.js
@@ -1,9 +1,42 @@
+import { motion } from "framer-motion";
+
 export const Design_Hamburger = ({ ShowHamburgerMenu }) => {
     return (
-        <div className="relative left-0 top-0 w-full h-full p-6 bg-myMainColor-300
-        ">
-            {/* 도와줘요 챗지피티!! 
-            애니메이션으로 하고싶어요 */}
+        <div>
+            <motion.svg
+                className="w-10 h-10 text-gray-500 cursor-pointer"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round">
+                {/* ShowHamburgerMenu == true : 햄버거 메뉴 오픈*/}
+                <motion.line
+                    x1="4"
+                    y1="6"
+                    x2="20"
+                    y2="6"
+                    animate={ShowHamburgerMenu ? { rotate: 45, y: 6 } : { rotate: 0, y: 0 }}
+                    transition={{ duration: 0.3 }}
+                />
+                <motion.line
+                    x1="4"
+                    y1="12"
+                    x2="20"
+                    y2="12"
+                    animate={ShowHamburgerMenu ? { scale:0 } : { scale:1 }}
+                    transition={{ duration: 0.3 }}
+                />
+                <motion.line
+                    x1="4"
+                    y1="18"
+                    x2="20"
+                    y2="18"
+                    animate={ShowHamburgerMenu ? { rotate: -45, y: -6 } : { rotate: 0, y: 0 }}
+                    transition={{ duration: 0.3 }}
+                />
+            </motion.svg>
 
             {/* {ShowHamburgerMenu === true ? 
             // w-6 h-6 : 크기 설정
