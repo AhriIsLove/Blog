6f22c577a6cfaaf9d36a986339493dc54fa8f7da
AhriIsLove <60705922+AhriIsLove@users.noreply.github.com>
2025-07-02 18:02:20 +0900

    Back Menu sample add
    서버
    - 메뉴 API 샘플 추가(성공)

--- back/blogAPI/src/main/java/com/al/blogAPI/controller/MainController.java
+++ back/blogAPI/src/main/java/com/al/blogAPI/controller/MainController.java
@@ -1,13 +1,25 @@
 package com.al.blogAPI.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.al.blogAPI.dto.MenuDTO;
 import com.al.blogAPI.service.MenuService;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/blog")
 public class MainController {
 	private final MenuService menuService;
+
+	@GetMapping("/menu")
+	public String testMenu() {
+	    return "메뉴 호출 성공";
+	}
 }
