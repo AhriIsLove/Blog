d1c68f0508f81fb459a4a21fdef2440ce9626a9a
AhriIsLove <60705922+AhriIsLove@users.noreply.github.com>
2025-07-03 17:43:00 +0900

    Front API add
    클라이언트
    - API 추가
    - Menu 가져오기

---
+++ front/src/api/MainAPI.js
@@ -0,0 +1,17 @@
+import axios from "axios";//Terminal : yarn add axios
+
+export const API_SERVER_HOST = 'http://localhost:8081';
+
+const prefix = `${API_SERVER_HOST}/blog`;
+
+//서버에게 목록 요청
+export const getMenu = async() => {
+    // axios : ajax와 비슷한 역할
+    // get방식 API 요청
+    // get방식은 URL로 요청됨
+    const res = await axios.get(`${prefix}/menu`);
+
+    console.log('getMenu res.data : ' , res.data);
+
+    return res.data;
+};
--- front/src/pages/Header_Main.js
+++ front/src/pages/Header_Main.js
@@ -2,15 +2,32 @@
 import '../index.css';
 import '../App.css';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 // 이미지
 import logo from '../images/logo.png';//로고
 // 디자인
 import HamburgerComponent from '../components/header/HamburgerComponent';
+import { getMenu } from '../api/MainAPI';
+
+//API : MenuDTO
+const initMenuDTO = [{
+    id: 0,
+    name: '',
+    parent_id:0
+}];
 
 // 헤더
 const Header_Main = () => {
+    //API : 메뉴
+    const [menuDTO, setMenuDTO] = useState([...initMenuDTO]);
+    useEffect(() => {
+        getMenu().then(data => {
+            setMenuDTO(data);
+        });
+    }, []);
+
+
     //상세메뉴 여부
     const [ShowDropMenu, setShowDropMenu] = useState(false);
     const showDropMenu = () => {
@@ -45,61 +62,23 @@ const Header_Main = () => {
                 - 자식태그가 group-hover사용 가능하게 함
                 - group-hover:w-full : hover시 w-full 적용 */}
                 <div className='menuContent' /> {/* 공백 */}
-                <div className='menuContent group' onMouseOver={showDropMenu} onMouseOut={hideDropMenu}>
-                    인물소개
-                    <span className='ani_line duration-300 group-hover:top-[95%] group-hover:opacity-100'></span>
-                    {/* 상세메뉴 */}
-                    <div className={`dropmenu transition-all duration-500 ease-in-out transform origin-top 
-                    ${ShowDropMenu ? "opacity-100 scale-y-100" : "opacity-0 scale-y-0"}`}>
-                        인물소개<br />
-                        인물소개<br />
-                        인물소개<br />
+
+                {/* 메뉴 */}
+                {menuDTO.map(menu => 
+                    <div className='menuContent group' 
+                    key={menu.id}
+                    onMouseOver={showDropMenu} onMouseOut={hideDropMenu}>
+                        {menu.name}
+                        <span className='ani_line duration-300 group-hover:top-[95%] group-hover:opacity-100'></span>
+                        {/* 상세메뉴 */}
+                        <div className={`dropmenu transition-all duration-500 ease-in-out transform origin-top 
+                        ${ShowDropMenu ? "opacity-100 scale-y-100" : "opacity-0 scale-y-0"}`}>
+                            인물소개<br />
+                            인물소개<br />
+                            인물소개<br />
+                        </div>
                     </div>
-                </div>
-                <div className='menuContent group' onMouseOver={showDropMenu} onMouseOut={hideDropMenu}>
-                    포트폴리오
-                    <span className='ani_line duration-300 group-hover:top-[95%] group-hover:opacity-100'></span>
-                    {/* 상세메뉴 */}
-                    <div className={`dropmenu transition-all duration-500 ease-in-out transform origin-top 
-                    ${ShowDropMenu ? "opacity-100 scale-y-100" : "opacity-0 scale-y-0"}`}>
-                        포트폴리오<br />
-                        포트폴리오<br />
-                        포트폴리오<br />
-                    </div>
-                </div>
-                <div className='menuContent group' onMouseOver={showDropMenu} onMouseOut={hideDropMenu}>
-                    공부
-                    <span className='ani_line duration-300 group-hover:top-[95%] group-hover:opacity-100'></span>
-                    {/* 상세메뉴 */}
-                    <div className={`dropmenu transition-all duration-500 ease-in-out transform origin-top 
-                    ${ShowDropMenu ? "opacity-100 scale-y-100" : "opacity-0 scale-y-0"}`}>
-                        공부<br />
-                        공부<br />
-                        공부<br />
-                    </div>
-                </div>
-                <div className='menuContent group' onMouseOver={showDropMenu} onMouseOut={hideDropMenu}>
-                    취미
-                    <span className='ani_line duration-300 group-hover:top-[95%] group-hover:opacity-100'></span>
-                    {/* 상세메뉴 */}
-                    <div className={`dropmenu transition-all duration-500 ease-in-out transform origin-top 
-                    ${ShowDropMenu ? "opacity-100 scale-y-100" : "opacity-0 scale-y-0"}`}>
-                        취미<br />
-                        취미<br />
-                        취미<br />
-                    </div>
-                </div>
-                <div className='menuContent group' onMouseOver={showDropMenu} onMouseOut={hideDropMenu}>
-                    커뮤니티
-                    <span className='ani_line duration-300 group-hover:top-[95%] group-hover:opacity-100'></span>
-                    {/* 상세메뉴 */}
-                    <div className={`dropmenu transition-all duration-500 ease-in-out transform origin-top 
-                    ${ShowDropMenu ? "opacity-100 scale-y-100" : "opacity-0 scale-y-0"}`}>
-                        커뮤니티<br />
-                        커뮤니티<br />
-                        커뮤니티<br />
-                    </div>
-                </div>
+                )}
 
                 <HamburgerComponent></HamburgerComponent>
             </nav>
