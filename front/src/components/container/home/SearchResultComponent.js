c6ff3a548a721ae80eb65cbd953bced6e06a4b20
AhriIsLove <60705922+AhriIsLove@users.noreply.github.com>
2025-07-08 18:03:18 +0900

    Front search Update
    클라이언트
    - 검색결과 Grid방식으로 수정중...

--- front/src/components/container/home/SearchResultComponent.js
+++ front/src/components/container/home/SearchResultComponent.js
@@ -2,7 +2,7 @@ const SearchResultComponent = () => {
     return (
         <div>
             <div className='mt-10 mb-5
-        text-2xl'>
+            text-2xl'>
                 'XXX'에 대한 검색결과 XXX건
             </div>
             {/* 
@@ -12,21 +12,21 @@ const SearchResultComponent = () => {
             - 하위메뉴
             - 바로가기
             */}
-            <div className='flex flex-row p-3 border-2'>
-                <div className="w-3/4">
-                    <div className="">
-                        상위메뉴
-                    </div>
-                    <div className="text-6xl">
-                        메뉴
-                    </div>
-                    <div className="">
-                        하위메뉴, 하위메뉴, 하위메뉴
-                    </div>
-                </div>
-                <div className="w-1/4">
-                    바로가기
-                </div>
+
+            {/* 4행 4열 */}
+            <div class="grid grid-cols-4 grid-rows-3 gap-4 p-4 border-2">
+                {/* 1행 */}
+                <div class="col-span-3 bg-blue-500 text-white text-center">상위메뉴</div>
+
+                <div class="row-span-4 bg-blue-600 text-white text-center">바로가기</div>
+
+                {/* 2행 */}
+                <div class="row-span-2 bg-blue-400">ㄴ</div>
+                <div class="col-span-2 bg-blue-500 text-white text-center">메뉴</div>
+
+                {/* 3행 */}
+                <div class="bg-blue-400">ㄴ</div>
+                <div class="bg-blue-400 text-center">하위메뉴</div>
             </div>
         </div>
     );
