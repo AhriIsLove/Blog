21a96de6874f2d7a1f28804966060dbad9ec2fc5
AhriIsLove <60705922+AhriIsLove@users.noreply.github.com>
2025-09-10 17:49:50 +0900

    PersonalPage Update
    PersonalPage Update

--- front/src/pages/portfolio/PersonalPage.js
+++ front/src/pages/portfolio/PersonalPage.js
@@ -1,8 +1,18 @@
 const PersonalPage = () => {
     //화면 리턴
     return (
-        <div>
-            PersonalPage
+        <div className="w-full 
+        flex flex-col items-center">
+            <div className="text-3xl font-bold text-myFontColor-950">
+                개인/팀 포트폴리오
+            </div>
+            
+            <div>
+                AssemERP
+                Righteous crime
+                MSTD(Metal Slug Tower Defence)
+                ???모모도라 러너???
+            </div>
         </div>
     );
 }
